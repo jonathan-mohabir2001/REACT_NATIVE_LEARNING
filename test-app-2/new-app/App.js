8ae794a6e1b77f9827d5fd921b9a3475aeb8f188
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-10-04 13:50:46 -0400

    first commit, learning react native

---
+++ test-app-2/new-app/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>
+        Hello world this is example code. 
+      </Text>
+    </View>
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

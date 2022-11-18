05259810d7d5e0f1be6241fb44a4e6aafa1062c7
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-18 13:23:10 -0500

    new commit

--- assignment_01/appliancesApp/App.js
+++ assignment_01/appliancesApp/App.js
@@ -13,15 +13,16 @@ return functions rather than just actions.
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+// provider is the component which will be wrapped around the App
 
-// constant of the url for the API above.
+import Home from './components/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <Home />
+    </Provider>
   );
 }
 
---
+++ assignment_01/appliancesApp/components/Home.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+function HomePage(){
+
+return(
+  <View>
+    <Text>Home Page here to loop through </Text>
+  </View>
+
+)
+
+}
